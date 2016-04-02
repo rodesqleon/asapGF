a458e6caba34ea241891136a4e9e46417706ba26
Rodrigo Esquivel <rodrigoe@moofwd.com>
2016-04-02 13:22:55 -0300

    Initial Commit

---
+++ asapGF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asapGF
+//
+//  Created by Rodrigo Esquivel on 02-04-16.
+//  Copyright © 2016 Rodrigo Esquivel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ asapGF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asapGF
+//
+//  Created by Rodrigo Esquivel on 02-04-16.
+//  Copyright © 2016 Rodrigo Esquivel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
